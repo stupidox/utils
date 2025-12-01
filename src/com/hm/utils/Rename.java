7659fe0031e81a05d89f31c85b64c57202c36648
“stupidox” <lukelelric@163.com>
2025-12-01 15:42:03 +0800

    重命名文件或文件夹

--- src/com/hm/utils/Rename.java
+++ src/com/hm/utils/Rename.java
@@ -1,20 +1,30 @@
 package com.hm.utils;
 
 import java.io.File;
-import java.util.regex.Pattern;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 
 public class Rename {
-    public static void main(String[] args) {
-        String path = "D:\\game\\RA2 Tools\\TNKD";
+    public static void main(String[] args) throws Exception {
+        String path = "D:\\private\\pictures\\best\\迪丽热巴\\000\\017";
         File file = new File(path);
         File[] files = file.listFiles();
-        for (File f : files) {
-            if (!f.isFile()) {
-                continue;
-            }
+        List<File> list = Arrays.asList(files);
+        list.sort(Comparator.comparing(File::getName));
+        for (int i = 0; i < list.size(); i++) {
+            File f = list.get(i);
             String name = f.getName();
-            name = name.replace("SUPTNKD", "TURRETTNKD");
-            f.renameTo(new File(f.getParent(), name));
+            if (f.isFile()) {
+                int j = name.lastIndexOf(".");
+                if (!name.substring(0, j).equals(String.format("%03d", i+1))) {
+                    System.out.println(name.substring(0, j) + " - " + String.format("%03d", i + 1));
+                    f.renameTo(new File(f.getParent(), String.format("%03d", i + 1) + name.substring(j)));
+                }
+            } else {
+                System.out.println(name + " - " + String.format("%03d", i+1));
+                f.renameTo(new File(f.getParent(), String.format("%03d", i+1)));
+            }
         }
     }
 }
