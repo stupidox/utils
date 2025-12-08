3b4015cba0c2a7159f640c0425957f8c684c63f3
“stupidox” <lukelelric@163.com>
2025-12-09 01:05:04 +0800

    复用重命名

--- src/com/hm/utils/Rename.java
+++ src/com/hm/utils/Rename.java
@@ -4,12 +4,20 @@ import java.io.File;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
 
 public class Rename {
     public static void main(String[] args) throws Exception {
-        String path = "D:\\private\\pictures\\best\\迪丽热巴\\000\\017";
+        renameFiles("D:\\private\\pictures\\best\\古力娜扎\\000\\096");
+    }
+
+    public static void renameFiles(String path) throws Exception {
+        System.out.println("\33[31m" + path + "\33[0m");
         File file = new File(path);
         File[] files = file.listFiles();
+        if (Objects.isNull(files) || files.length == 0) {
+            return;
+        }
         List<File> list = Arrays.asList(files);
         list.sort(Comparator.comparing(File::getName));
         for (int i = 0; i < list.size(); i++) {
